356a2325ce58383b7fd1efb4bff1ebda24101f1c
ammarabdulqayoom <69830329+ammarabdulqayoom@users.noreply.github.com>
2025-08-03 15:10:07 +0500

    Add files via upload

---
+++ v53 netflix clone/script.js
@@ -0,0 +1,54 @@
+
+// const button = 
+// document.querySelector(' .btn btn-red-sn');
+
+// button.addEventListener( 'click', 
+//     function() {
+
+//     document.getElementById('massage').textContent = "Submitted!";
+//     prompt(massage);
+// });
+
+
+// const button1 = 
+// document.querySelector(' .btn btn-red-sn');
+
+// button.addEventListener( 'click', 
+//     function() {
+
+//     document.getElementById('massage').textContent = "Submitted!";
+//     prompt(massage);
+// });
+
+
+// let arr = [20, 50, 3, 40, 1];
+// let largestN = arr[0];
+
+// for (i = 1; i < arr.length; i++) {
+//     if (arr[i] > largestN) {
+//         largestN = arr[i];
+
+//     }
+// }
+// console.log(largestN);
+
+// let arr1 = [20, 50, 3, 40, -1, 99];
+// let smalestN = arr1[0];
+
+// for (i = 1; i < arr1.length; i++) {
+//     if (arr1[i] < smalestN) {
+//         smalestN = arr1[i];
+
+//     }
+// }
+// console.log(smalestN);
+
+const arr2 = [20, 50, 3, 40, -1, 99];
+let evenN = [];
+
+for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] % 2 === 0) 
+        evenN = (arr2[i])
+}
+console.log(evenN);
+
---
+++ v53 netflix clone/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let arr1 = [20, 50, 3, 40, -1, 99];
+// let smalestN = arr1[0];
+
+// for (i = 1; i < arr1.length; i++) {
+//     if (arr1[i] < smalestN) {
+//         smalestN = arr1[i];
+
+//     }
+// }
